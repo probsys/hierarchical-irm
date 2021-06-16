37753979f3f29f5dd5ba1704cabcd11956d6ac49
Feras A. Saad <fsaad@mit.edu>
2021-06-15 20:58:59 -0400

    Pin version 0.1.1.

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2021 MIT Probabilistic Computing Project
 # Apache License, Version 2.0, refer to LICENSE.txt
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .hirm import IRM
 from .hirm import HIRM
